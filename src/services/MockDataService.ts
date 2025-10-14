5639719943d75591143e74ec9947df7b5d953d1f
Ricky <ricky.georgius@gmail.com>
2025-10-15 06:50:43 +0700

    update: mock Data

--- src/services/MockDataService.ts
+++ src/services/MockDataService.ts
@@ -1,4 +1,4 @@
-import type { TaskModel } from '../models/TaskModel';
+import { TaskModel } from '../models/TaskModel';
 import { UserModel } from '../models/UserModel';
 
 export class MockDataService {
@@ -11,7 +11,60 @@ export class MockDataService {
     }),
   ];
 
-  private static _tasks: TaskModel[] = [];
+  private static _tasks: TaskModel[] = [
+    new TaskModel({
+      id: 'task_1760484360103_stu564ue9',
+      title: 'User Management',
+      description: '',
+      isCompleted: true,
+      priority: 'medium',
+      dueDate: '2025-10-15',
+      assignedTo: 'user_1',
+      createdAt: '2025-10-14T23:26:00.103Z',
+      updatedAt: '2025-10-14T23:26:03.645Z',
+      isDeleted: false,
+      deletedAt: null,
+    }),
+    new TaskModel({
+      id: 'task_1760484402390_v197irsva',
+      title: 'Task List',
+      description: 'Show task list and divide based on the stats',
+      isCompleted: true,
+      priority: 'high',
+      dueDate: '2025-10-16',
+      assignedTo: 'user_1',
+      createdAt: '2025-10-14T23:26:42.390Z',
+      updatedAt: '2025-10-14T23:26:45.269Z',
+      isDeleted: false,
+      deletedAt: null,
+    }),
+    new TaskModel({
+      id: 'task_1760484436369_qnjifs7jh',
+      title: 'Integrate Backend API',
+      description: 'Waiting for the API from backend',
+      isCompleted: false,
+      priority: 'high',
+      dueDate: '2025-10-31',
+      assignedTo: 'user_1',
+      createdAt: '2025-10-14T23:27:16.369Z',
+      updatedAt: '2025-10-14T23:27:16.369Z',
+      isDeleted: false,
+      deletedAt: null,
+    }),
+    new TaskModel({
+      id: 'task_1760484456317_9j7ifc33l',
+      title: 'Documentation',
+      description: '',
+      isCompleted: false,
+      priority: 'medium',
+      dueDate: '2025-10-14',
+      assignedTo: 'user_1',
+      createdAt: '2025-10-14T23:27:36.317Z',
+      updatedAt: '2025-10-14T23:27:36.317Z',
+      isDeleted: false,
+      deletedAt: null,
+    }),
+  ];
 
   static getUsers(): UserModel[] {
     return this._users;
